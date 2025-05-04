5fccd62b1c70975b8fc3dc2c43c30d488e79c7c2
Jae-il <pkyj040410@gmail.com>
2025-05-04 14:19:04 +0900

    Update CalculatorLV1.java

--- CalculatorProject/src/com/example/calculatorLV1/CalculatorLV1.java
+++ CalculatorProject/src/com/example/calculatorLV1/CalculatorLV1.java
@@ -1,5 +1,6 @@
 package com.example.calculatorLV1;
 import java.util.Scanner;
+import java.util.Set;
 
 public class CalculatorLV1 {
     public static void main(String[] args) {
@@ -16,7 +17,7 @@ public class CalculatorLV1 {
             second_int = sc.nextInt();
             System.out.print("\n사칙 연산 기호를 입력해주세요(+, -, *, /): ");
             char operations = sc.next().charAt(0);
-            
+
             if(first_int<0 || second_int<0) {
                 System.out.println("입력값은 0 이상의 정수이어야 합니다. 다시 입력해주세요!!");
                 continue;
@@ -24,7 +25,7 @@ public class CalculatorLV1 {
                 System.out.println("연산 기호는 +, -, *, / 중에 하나이어야 합니다. 다시 입력해주세요!!");
                 continue;
             }
-            
+
             switch (operations) {
                 case '+':
                     result = first_int + second_int;
